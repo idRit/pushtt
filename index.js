7cffadcef1a63868b81f6fbf80043b248f599cd3
Ritwik Mukherjee <mritwik369@gmail.com>
2019-09-03 12:50:21 +0530

    b

--- index.js
+++ index.js
@@ -32,14 +32,14 @@ io.on('connection', (socket) => {
         console.log(room);
     });    
 
-    socket.on('msg', (msg) => {
+    socket.on('#msg', (msg) => {
         console.log(msg.msg);
         io.sockets.in(msg.room).emit(msg.msg);
     });
 
     socket.on('getUsers', (roomName) => {
         io.of('/').in(roomName).clients((err, data) => {
-            io.emit('users', data);
+            io.emit('#users', data);
         });
     })
 
