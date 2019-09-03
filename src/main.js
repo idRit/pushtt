1cacc2bc562ad348ef7fd05302efc47452e51227
Ritwik Mukherjee <mritwik369@gmail.com>
2019-09-03 12:30:59 +0530

    jgvkbh

--- src/main.js
+++ src/main.js
@@ -5,10 +5,20 @@ import store from './store'
 import './registerServiceWorker'
 import VueSocketIO from 'vue-socket.io'
 
-Vue.use(VueSocketIO, location.protocol +
-  "//" +
-  location.hostname +
-  (location.port ? ":" + location.port : "8080"));
+// Vue.use(VueSocketIO, location.protocol +
+//   "//" +
+//   location.hostname +
+//   (location.port ? ":" + location.port : "8080"));
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'https://pushtok.herokuapp.com',
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_'
+  },
+}));
 
 Vue.config.productionTip = false
 
